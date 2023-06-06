774f81fc47b0523e8327ee5541d5dd6d33814ee1
RomanKyryllov <roma.mistaffer@gmail.com>
2023-06-06 18:09:51 +0300

    Homework 1: for, while, do-while.

--- src/main/java/lesson_1/Main.java
+++ src/main/java/lesson_1/Main.java
@@ -2,6 +2,33 @@ package lesson_1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("first_lesson");
+        int[] ar = {0, 1, 2, 3, 4};
+
+        for (int i = 0; i < ar.length; i++) {
+            if (i == ar.length - 1)
+                System.out.print(ar[i]);
+            else
+                System.out.print(ar[i] + ", ");
+        }
+        System.out.println();
+
+        int e = 0;
+        while (e < ar.length) {
+            if (e == ar.length - 1)
+                System.out.print(ar[e]);
+            else
+                System.out.print(ar[e] + ", ");
+            e++;
+        }
+        System.out.println();
+
+        int k = 0;
+        do {
+            if (k == ar.length - 1)
+                System.out.print(ar[k]);
+            else
+                System.out.print(ar[k] + ", ");
+            k++;
+        } while (k < ar.length);
     }
 }
