133f8982d52c920f2fbeaba8018f0caf1a89e9f0
RomanKyryllov <roma.mistaffer@gmail.com>
2023-06-07 18:38:46 +0300

    Homework 2: switch case

--- src/main/java/lesson_1/Main.java
+++ src/main/java/lesson_1/Main.java
@@ -2,33 +2,21 @@ package lesson_1;
 
 public class Main {
     public static void main(String[] args) {
-        int[] ar = {0, 1, 2, 3, 4};
+        int a = (int) (Math.random()*5);
 
-        for (int i = 0; i < ar.length; i++) {
-            if (i == ar.length - 1)
-                System.out.print(ar[i]);
-            else
-                System.out.print(ar[i] + ", ");
+        switch (a){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 4:
+                System.out.println("4");
+                break;
         }
-        System.out.println();
-
-        int e = 0;
-        while (e < ar.length) {
-            if (e == ar.length - 1)
-                System.out.print(ar[e]);
-            else
-                System.out.print(ar[e] + ", ");
-            e++;
-        }
-        System.out.println();
-
-        int k = 0;
-        do {
-            if (k == ar.length - 1)
-                System.out.print(ar[k]);
-            else
-                System.out.print(ar[k] + ", ");
-            k++;
-        } while (k < ar.length);
     }
 }
