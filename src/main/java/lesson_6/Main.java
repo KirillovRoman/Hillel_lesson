52f6f72de0e80af6abc52916b420a65a484629ad
RomanKyryllov <roma.mistaffer@gmail.com>
2023-06-25 15:27:48 +0300

    Homework 6: Exceptions and try with resources

--- src/main/java/lesson_6/Main.java
+++ src/main/java/lesson_6/Main.java
@@ -1,22 +1,22 @@
 package lesson_6;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        try {
-            exceptions();
-        } catch (Exception e) {
-            System.out.println("Caught exception in catch block: " + e.getMessage());
-        }
+        method();
     }
 
-    public static void exceptions() throws Exception {
-        try {
+    public static void method() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.println("Entered number: " + number);
+
             throw new Exception("Exception in try block");
         } catch (Exception e) {
-            throw new Exception("Exception in catch block");
-        } finally {
-            throw new Exception("Exception in try-catch with resources block");
+            System.out.println("Catch block");
+            throw new RuntimeException("Exception in catch block");
         }
     }
 }
